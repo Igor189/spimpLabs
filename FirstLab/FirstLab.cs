ef9648bd3cc4c0e2c05e3b3e4fd40c516ed0f9e0
shabardin.i <igorshabardin42@gmail.com>
2023-09-22 20:43:42 +0300

    feat: add firstLab project

---
+++ FirstLab/FirstLab.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var partCount = 3;
+
+var result = numbers
+    .Select((number, i) => new { Index = i, Value = number })
+    .GroupBy(number => number.Index / partCount)
+    .Select(partList => partList.Sum(number => number.Value));
+
+foreach (var num in result)
+{
+    Console.WriteLine(num);
+}
